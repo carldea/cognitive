3ea8b1a1da6956275403967b0d6de11f45b2a155
Carl Dea <cardea@deloitte.com>
2024-06-23 22:52:09 -0400

    Feature #2 - New support for property identifier lookup and validation. This allows fields to reference a data object instead of just a property name as a string. Add license to files.

--- src/test/java/org/carlfx/cognitive/test/demo/AccountCreateController.java
+++ src/test/java/org/carlfx/cognitive/test/demo/AccountCreateController.java
@@ -1,6 +1,22 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright © 2023-2024 Carl Dea.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.carlfx.cognitive.test.demo;
 
-import javafx.beans.property.BooleanProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -10,7 +26,6 @@ import javafx.scene.control.Tooltip;
 import javafx.scene.text.Text;
 import org.carlfx.cognitive.loader.InjectViewModel;
 import org.carlfx.cognitive.validator.ValidationMessage;
-import org.carlfx.cognitive.viewmodel.ValidationViewModel;
 
 import static org.carlfx.cognitive.test.demo.AccountViewModel.*;
 
